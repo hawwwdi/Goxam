7c10eb0a6300b96be00829df1ebdd58a08c79e60
hadi <hawwwdi@gmail.com>
2020-09-04 16:45:33 +0430

    develope NewOption function

--- internal/models/question/option.go
+++ internal/models/question/option.go
@@ -1,7 +1,18 @@
 package question
 
+import "image"
+
 type Option struct {
 	Description
+	Value bool
+}
+
+func NewOption(txt []rune, img image.Image, value bool) Option {
+	o := new(Option)
+	o.Text = txt
+	o.Picture = img
+	o.Value = value
+	return *o
 }
 
 func (o *Option) Form() string {
