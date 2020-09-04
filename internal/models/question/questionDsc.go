37f8af27b2683db17bb3b262c049b33f21a775ca
hadi <hawwwdi@gmail.com>
2020-09-04 16:15:14 +0430

    develope questionDsc

--- internal/models/question/questionDsc.go
+++ internal/models/question/questionDsc.go
@@ -1,1 +1,18 @@
 package question
+
+// Dsc is alias for description
+type Dsc struct {
+	Description
+}
+
+func (d *Dsc) Form() string {
+	//todo
+	return "this is form"
+}
+
+func (d *Dsc) String() string {
+	//todo
+	return "this is string"
+}
+
+
