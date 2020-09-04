8a9466b12bc74f6af1d009dcb40a937dfc2a9eae
hadi <hawwwdi@gmail.com>
2020-09-04 17:12:32 +0430

    develope newTest function

--- internal/models/question/testQuestion.go
+++ internal/models/question/testQuestion.go
@@ -1,7 +1,17 @@
 package question
 
 type Test struct {
+	Dsc
 	MultipleChoice bool
+	Options []Option
+}
+
+func NewTest(dsc Description, mc bool) Test {
+	t := new(Test)
+	t.Description = dsc
+	t.MultipleChoice = mc
+	t.Options = make([]Option, 0, 2)
+	return *t
 }
 
 func (t *Test) CheckAnswer(answer ...string) bool {
