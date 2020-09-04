6267e4bd7a88d5c48f2b3cc8615ed98d14435808
hadi <hawwwdi@gmail.com>
2020-09-04 16:01:05 +0430

    develpe option

---
+++ internal/models/question/option.go
@@ -0,0 +1,15 @@
+package question
+
+type Option struct {
+	Description
+}
+
+func (o *Option) Form() string {
+	//todo
+	return "this is form"
+}
+
+func (o *Option) String() string {
+	//todo
+	return "this is string"
+}
