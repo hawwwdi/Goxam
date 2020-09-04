e7df9078a886972187e4aa8439e67967e9005523
hadi <hawwwdi@gmail.com>
2020-09-04 15:54:03 +0430

    add description struct

--- internal/models/question/commons.go
+++ internal/models/question/commons.go
@@ -1,10 +1,16 @@
 package question
 
+import "image"
+
 type Question interface {
 	CheckAnswer(...string) bool
 	String() string
 	Form() string
 }
 
+type Description struct {
+	Text []rune
+	Picture image.Image
+}
 
 
