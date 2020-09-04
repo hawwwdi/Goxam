cda77d2f884e5ce2a3b2c01fee4d79eaa404a1fc
hadi <hawwwdi@gmail.com>
2020-09-04 12:10:00 +0430

    develope test type

---
+++ internal/models/question/test.go
@@ -0,0 +1,20 @@
+package question
+
+type test struct {
+
+}
+
+func (t *test) CheckAnswer(answer string) bool {
+	//todo
+	return true
+}
+
+func (t *test) ToForm () string {
+	//todo
+	return "this is form"
+}
+
+func (t *test) String() string {
+	//todo
+	return "this is string"
+}
