eb7e083c2f72edcf017628683c9c6e2b1a0ccaa8
hadi <hawwwdi@gmail.com>
2020-09-05 16:16:31 +0430

    develope exam type and New function

--- internal/models/exam/commons.go
+++ internal/models/exam/commons.go
@@ -1,11 +1,26 @@
 package exam
 
-import "time"
+import (
+	"github.com/gofrs/uuid"
+	"github.com/hawwwdi/Goxam/internal/models/question"
+	"time"
+)
 
 type Exam struct {
-	id string
+	question.Description
+	id uuid.UUID
 	time time.Duration
 }
 
+func NewExam(dsc question.Description, time time.Duration) Exam {
+	e := new(Exam)
+	e.Description = dsc
+	e.time = time
+	e.id = uuid.Must(uuid.NewV1())
+	return *e
+}
 
+func (e *Exam) AddQuestion(question question.Question) {
+
+}
 
