d71de2653cd2a5689728f44f6272d69a2251fa2c
hadi <hawwwdi@gmail.com>
2020-09-06 09:25:33 +0430

    add some methods to the exam

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hawwwdi/Goxam
 
 go 1.14
+
+require github.com/rs/xid v1.2.1 // indirect
--- internal/models/exam/commons.go
+++ internal/models/exam/commons.go
@@ -1,14 +1,14 @@
 package exam
 
 import (
-	"github.com/gofrs/uuid"
 	"github.com/hawwwdi/Goxam/internal/models/question"
+	"github.com/rs/xid"
 	"time"
 )
 
 type Exam struct {
 	question.Description
-	id uuid.UUID
+	id string
 	time time.Duration
 }
 
@@ -16,11 +16,19 @@ func NewExam(dsc question.Description, time time.Duration) Exam {
 	e := new(Exam)
 	e.Description = dsc
 	e.time = time
-	e.id = uuid.Must(uuid.NewV1())
+	e.id = xid.New().String()
 	return *e
 }
 
 func (e *Exam) AddQuestion(question question.Question) {
+	//todo save question with exam id in database
+}
 
+func (e *Exam) RemQuestion(index int) {
+	//todo remove question from database
+}
+
+func (e *Exam) SetTime(duration time.Duration){
+	e.time = duration
 }
 
