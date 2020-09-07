35f627e6858dc9fbea07dbe43a6367cf5a871d02
ARMIN <armingodarzi1380@gmail.com>
2020-09-07 13:26:16 +0430

    seeRequest func added for teacherClass

--- internal/models/class/teacherClass.go
+++ internal/models/class/teacherClass.go
@@ -3,6 +3,7 @@ package class
 func (class TeacherClass) Handle() {
 	createExam(class)
 	seeResults(class)
+	seeResults(class)
 }
 
 //TODO creating exam
@@ -11,4 +12,8 @@ func createExam(class TeacherClass) {
 
 //TODO showing results of test
 func seeResults(class TeacherClass) {
+}
+//TODO see request of chosen class
+func seeRequests(class TeacherClass)  {
+
 }
