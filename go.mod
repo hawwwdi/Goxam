1ee79844232eca3a6494b9efdf9658e20e57f013
hadi <hawwwdi@gmail.com>
2020-09-09 02:42:04 +0430

    add testing for SaveExam, status: FAIL

--- go.mod
+++ go.mod
@@ -2,11 +2,7 @@ module github.com/hawwwdi/Goxam
 
 go 1.14
 
-<<<<<<< HEAD
-require github.com/rs/xid v1.2.1 // indirect
-=======
 require (
-	github.com/go-sql-driver/mysql v1.5.0
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 )
->>>>>>> armin
---
+++ internal/db/dbHandler/examHandler_test.go
@@ -0,0 +1,16 @@
+package dbHandler
+
+import (
+	"github.com/hawwwdi/Goxam/internal/models/exam"
+	"github.com/hawwwdi/Goxam/internal/models/question"
+	"testing"
+	"time"
+)
+
+func TestSaveExam(t *testing.T) {
+	dsc := question.NewDescription(`this is a test`, "test")
+	exm := exam.NewExam(dsc, time.Now(), time.Hour, "123", 3)
+	if err := SaveExam(&exm, "test"); err != nil {
+		t.Error(err)
+	}
+}
