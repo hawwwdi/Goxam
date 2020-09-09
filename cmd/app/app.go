7c3ca02a708c1536edfb775b13167aff16ae8e8d
ARMIN <armingodarzi1380@gmail.com>
2020-09-09 20:26:29 +0430

    starting changing the teacher and user structure

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	//"github.com/gin-gonic/gin"
 	"github.com/hawwwdi/Goxam/internal/models/start"
 )
 
 func main() {
-	start.Start()
+	go 	start.Start()
+	/*r := gin.Default()
+	r.LoadHTMLGlob("views/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200,"index.tmpl","dfa")
+	})
+	_ = r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")*/
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/hawwwdi/Goxam
 go 1.14
 
 require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 )
