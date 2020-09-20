c2e864229ef0ef611e7a153077ae44c57fdd24c8
hadi <hawwwdi@gmail.com>
2020-09-21 01:49:26 +0330

    develope image struct and save function

--- internal/imageMangement/image.go
+++ internal/imageMangement/image.go
@@ -1,1 +1,41 @@
 package imageMangement
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"time"
+)
+
+const savePath = "../images/"
+
+type Image struct {
+	id   int64
+	path string
+}
+
+// pass teacher, exam, question/student name + fileName as pathParts
+func NewImage(img *os.File, pathParts ...string) (*Image, error) {
+	i := new(Image)
+	i.id = currTime()
+	i.path = path.Join(pathParts...)
+	err := saveImg(img, i.path)
+	if err != nil {
+		return nil, err
+	}
+	return i, nil
+}
+
+func saveImg(file *os.File, imgPath string) error {
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+	err1 := ioutil.WriteFile(imgPath, bytes, os.ModeType)
+	//todo check above code
+	return err1
+}
+
+func currTime() int64 {
+	return time.Now().UnixNano() / int64(time.Millisecond)
+}
